57447f2687a7358a0d573721bc4a61f06858b643
Markos Rafael <escalante.markos18@gmail.com>
2020-10-23 23:57:10 -0700

    create webpage and styling and js file

---
+++ script.js
@@ -0,0 +1,27 @@
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
+  var userInput = confirm("Would you like to create a new and safe password?");
+  var userInput = confirm("Your password must be at least 8 characters and must not exceed 128 characters!");
+  var userInput = confirm("Would you like to use a lowercase letter?");
+  var userInput = confirm("Would you like to use an uppercase letter?");
+  var userInput = confirm("Would you like to use a number?");
+  var userInput = confirm("Would you like to use a symbol?");
+  
+  // Write password to the #password input
+  // Add event listener to generate button
+  
+  generateBtn.addEventListener("click", writePassword);
+  
+  function generateBtn() {
+    var create = document.getElementById("#generate").value;
+  
+    var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+[]{}/?";
+    var password = "";
+    for (var i =0; i <= create; i++) {
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+  
+  
+    document.getElementById("#display").value = password;
+  
+  }
